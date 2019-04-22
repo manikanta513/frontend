24f3820381bf9b0fce48d0385d302e7e5b36830b
root <root@docker.c.woven-acolyte-203810.internal>
2019-04-22 05:32:42 +0000

    my first projrct

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            hi this is manikanta.how r u
           </p>
           <a
             className="App-link"
